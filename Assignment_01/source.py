b0b22dcd515e21b73529a51a240b4adf67a2b0f9
tejasvaidya94 <tejasvaidya123580@gmail.com>
2025-10-06 12:49:36 +0530

    Code is implemented to convert the bgr image into grayscale

---
+++ Assignment_01/source.py
@@ -0,0 +1,26 @@
+# Q.Convert the image to grayscale using the opencv.
+
+import cv2 
+
+# read the image using cv2
+image = cv2.imread('sample.jpg')
+# check if any error while loading the image.
+if image is None:
+    print("Error: Image does not found.")
+    exit()
+
+# Display original image
+cv2.imshow('Original Image',image)
+cv2.waitKey(0)
+
+# Convert to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Display modified image
+cv2.imshow('Grayscale Image',gray_image)
+cv2.wait(0)
+cv2.destroyAllWindows()
+
+# Save the modified image
+cv2.imwrite('converted_image.jpg',gray_image)
+
