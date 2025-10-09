32c78c59c14ce0faf79354557fe70c1dabaaa277
tejasvaidya94 <tejasvaidya123580@gmail.com>
2025-10-09 14:33:23 +0530

    Formated the code in source.py inside the Assignment_03 directory.

--- Assignment_03/source.py
+++ Assignment_03/source.py
@@ -16,5 +16,10 @@ _, thresh = cv2.threshold(image, 128, 240, cv2.THRESH_BINARY)
 # display original and converted image.
 cv2.imshow("Original Image", image)
 cv2.imshow("Thresholded Rectangle", thresh)
+
+# wait and then close window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Save the modified image
+cv2.imwrite('./Assignment_03/converted_image.jpg', thresh)
