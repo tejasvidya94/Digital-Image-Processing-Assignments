e7c1025e48fbd09038193ee263868a748dd26f85
tejasvaidya94 <tejasvaidya123580@gmail.com>
2025-10-09 14:29:07 +0530

    Implemented the code for removing all the components other than rectangle using openCV's thresholding in Assignment_03.

---
+++ Assignment_03/source.py
@@ -0,0 +1,20 @@
+# Q.Separate the rectangle from the image using the thresholding from openCV.
+import cv2
+import numpy as np
+
+# Read the image in grayscale
+image = cv2.imread('./Assignment_03/sample.png', cv2.IMREAD_GRAYSCALE)
+
+# Check if image is loaded
+if image is None:
+    print("Error: Wrong path")
+    exit()
+
+# use _ to store the thresold value and thresh for storing the resultant image
+_, thresh = cv2.threshold(image, 128, 240, cv2.THRESH_BINARY)
+
+# display original and converted image.
+cv2.imshow("Original Image", image)
+cv2.imshow("Thresholded Rectangle", thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
