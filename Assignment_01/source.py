e569110155f7852b110179fe3fa36dd6396f02a1
tejasvaidya94 <tejasvaidya123580@gmail.com>
2025-10-09 14:08:56 +0530

    Formated code

--- Assignment_01/source.py
+++ Assignment_01/source.py
@@ -1,6 +1,6 @@
 # Q.Convert the image to grayscale using the opencv.
 
-import cv2 
+import cv2
 
 # read the image using cv2
 image = cv2.imread('sample.jpg')
@@ -10,17 +10,16 @@ if image is None:
     exit()
 
 # Display original image
-cv2.imshow('Original Image',image)
+cv2.imshow('Original Image', image)
 cv2.waitKey(0)
 
 # Convert to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Display modified image
-cv2.imshow('Grayscale Image',gray_image)
+cv2.imshow('Grayscale Image', gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Save the modified image
-cv2.imwrite('converted_image.jpg',gray_image)
-
+cv2.imwrite('converted_image.jpg', gray_image)
